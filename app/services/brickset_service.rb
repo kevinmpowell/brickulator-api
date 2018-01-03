804f1a150850957de8e0a451c3a84f04fec4cb93
Kevin Powell <kevinmpowell@gmail.com>
2018-01-02 20:31:41 -0600

    Update Brickset Service again

--- app/services/brickset_service.rb
+++ app/services/brickset_service.rb
@@ -7,9 +7,8 @@ class BricksetService
   THIRTY_DAYS_IN_MINUTES = TWENTY_FOUR_HOURS_IN_MINUTES * 30
   ONE_YEAR_IN_MINUTES = TWENTY_FOUR_HOURS_IN_MINUTES * 365
 
-  def self.get_sets updated_minutes_ago = nil
-    minutes_ago = updated_minutes_ago || ONE_YEAR_IN_MINUTES
-    uri = URI.parse("https://brickset.com/api/v2.asmx/getRecentlyUpdatedSets?apiKey=#{API_KEY}&minutesAgo=#{minutes_ago}")
+  def self.get_sets
+    uri = URI.parse("https://brickset.com/api/v2.asmx/getRecentlyUpdatedSets?apiKey=#{API_KEY}&minutesAgo=#{ONE_YEAR_IN_MINUTES}")
     response = Net::HTTP.get_response(uri)
     sets = Hash.from_xml(response.body)
     sets
