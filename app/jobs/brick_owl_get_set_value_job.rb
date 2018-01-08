6e87c8311947e23f6195c5d0a2ce92951fa499f2
Kevin Powell <kevinmpowell@gmail.com>
2018-01-07 20:25:57 -0600

    WIP individual BO value getter job

--- app/jobs/brick_owl_get_set_value_job.rb
+++ app/jobs/brick_owl_get_set_value_job.rb
@@ -9,14 +9,19 @@ class BrickOwlGetSetValueJob < ActiveJob::Base
     s = LegoSet.find(id)
     
     unless ls.nil?
-      puts "Getting Brick Owl Values for #{s.number}-#{s.number_variant}"
-      brick_owl_values = BrickOwlService.get_values_for_set(s)
-      if brick_owl_values.empty?
-        puts "No BO data to save for #{s.number}-#{s.number_variant}"
-      else
-        brick_owl_values[:lego_set_id] = s.id
-        brick_owl_values[:retrieved_at] = Time.now
-        BrickOwlValue.create(brick_owl_values)
+      begin
+        puts "Getting Brick Owl Values for #{s.number}-#{s.number_variant}"
+        brick_owl_values = BrickOwlService.get_values_for_set(s)
+        if brick_owl_values.empty?
+          puts "No BO data to save for #{s.number}-#{s.number_variant}"
+        else
+          brick_owl_values[:lego_set_id] = s.id
+          brick_owl_values[:retrieved_at] = Time.now
+          BrickOwlValue.create(brick_owl_values)
+        end
+      rescue Exception => e
+        
+        raise e
       end
     end
 
