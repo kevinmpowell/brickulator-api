9997e536206c44522759134546d6a34d7b6c3fe2
Kevin Powell <kevinmpowell@gmail.com>
2018-01-19 08:07:15 -0600

    WIP: Bricklink data retrieval

--- app/services/bricklink_service.rb
+++ app/services/bricklink_service.rb
@@ -4,40 +4,164 @@ require 'open-uri'
 
 class BricklinkService
   BASE_URL = 'https://www.bricklink.com/catalogPG.asp?S='
+  BASE_PART_OUT_URL = 'https://www.bricklink.com/catalogPOV.asp?itemType=S&itemNo='
 
   def self.c_to_f string
     # Currency string to float
-    puts string.gsub(/~|US|,|\$/, "")
-    string.gsub(/~|US|,|\$/, "").to_f.round(2)
+    string.gsub(/~|US|,|\$|[[:space:]]/, "").to_f.round(2)
   end
 
 
-  def self.heartbeat set_number
+  def self.get_values_for_set s
+    set_number = "#{s.number}-#{s.number_variant}"
+    bricklink_values = {}
     url = "#{BASE_URL}#{set_number}"
-    puts url
+    # puts url
     doc = Nokogiri::HTML(open(url))
     set_listings = doc.css("table.fv tr:nth-child(4)")
-    current_new_listing_rows = set_listings.css("td:nth-child(3) table:nth-child(3) table tr")
-    # puts current_new_listing_rows
 
+    # NEW Complete Sets
+    new_set_data = BricklinkService.get_complete_set_new_values(set_listings)
+    bricklink_values = bricklink_values.merge(new_set_data)
+    # USED Complete Sets
+    used_set_data = BricklinkService.get_complete_set_used_values(set_listings)
+    bricklink_values = bricklink_values.merge(used_set_data)
+
+    # NEW Last 6 months sold
+    new_set_sold_data = BricklinkService.get_complete_set_last_six_months_sales_new_values(set_listings)
+    bricklink_values = bricklink_values.merge(new_set_sold_data)
+
+    # USED Last 6 months sold
+    used_set_sold_data = BricklinkService.get_complete_set_last_six_months_sales_used_values(set_listings)
+    bricklink_values = bricklink_values.merge(used_set_sold_data)
+
+
+    puts bricklink_values.to_yaml
+    bricklink_values
+  end
+
+  def self.get_part_out_values s, condition="new", minifigs="whole", include_instructions=true, include_box=true, include_extra_parts=true
+    condition = condition == "new" ? "N" : "U"
+    minifigs = minifigs == "whole" ? "M" : "P"
+    include_instructions ? "Y" : "N"
+    include_box = include_box ? "Y" : "N"
+    include_extra_parts = include_extra_parts ? "Y" : "N"
+
+    url = "#{BASE_PART_OUT_URL}#{s.number}&itemType=S&itemSeq=#{s.number_variant}&itemQty=1&breakType=#{minifigs}&itemCondition=#{condition}&incInstr=#{include_instructions}&incBox=#{include_box}&incParts=#{include_extra_parts}"
+    puts url
+  end
+
+  def self.get_complete_set_new_values set_listings
+    data = {}
+    current_new_listing_rows = set_listings.css("td:nth-child(3) table:nth-child(3) table tr")
     new_set_prices = []
     current_new_listing_rows.drop(1).each do |r|
       first_column = r.css("td:first-child")
-      if !first_column.attribute("colspan").nil? && first_column.attribute("colspan") == 3
-        break
-      else
+      break if !first_column.attribute("colspan").nil? && first_column.attribute("colspan").value.to_i == 3        
+
+      qty = r.css("td:nth-child(2)").text.to_i
+      value = BricklinkService.c_to_f(r.css("td:last-child").text)
+      qty.times do 
+        new_set_prices << value
+      end
+    end
+    # new_set_prices
+    data[:complete_set_new_listings_count] = 0
+    unless new_set_prices.empty?
+      data[:complete_set_new_listings_count] = new_set_prices.count
+      data[:complete_set_new_avg_price] = new_set_prices.mean.round(2)
+      data[:complete_set_new_median_price] = new_set_prices.median.round(2)
+      data[:complete_set_new_high_price] = new_set_prices.max
+      data[:complete_set_new_low_price] = new_set_prices.min
+    end
+    data
+  end
+
+  def self.get_complete_set_last_six_months_sales_new_values set_listings
+    data = {}
+    sold_new_listing_tables = set_listings.css("td:nth-child(1) table")
+    new_set_sold_prices = []
+
+    sold_new_listing_tables.each do |table|
+      first_cell = table.css("td:first-child").first
+      next if (!first_cell.attribute("colspan").nil? && first_cell.attribute("colspan").value.to_i == 3) || table.attribute("cellpadding").value.to_i == 0
+      sold_listing_rows = table.css("table tr")
+      sold_listing_rows.drop(1).each do |r|
+        first_column = r.css("td:first-child")
+        break if !first_column.attribute("colspan").nil? && first_column.attribute("colspan").value.to_i == 3        
         qty = r.css("td:nth-child(2)").text.to_i
-        # puts r.css("td:last-child").text
         value = BricklinkService.c_to_f(r.css("td:last-child").text)
         qty.times do 
-          new_set_prices << value
+          new_set_sold_prices << value
         end
       end
     end
-    puts new_set_prices.sort.to_yaml
-    # puts parsed_values.to_yaml
-    # puts table
-    # sets = Hash.from_xml(response.body)
-    # puts sets.to_yaml
+
+    data[:complete_set_completed_listing_new_listings_count] = 0
+    unless new_set_sold_prices.empty?
+      data[:complete_set_completed_listing_new_listings_count] = new_set_sold_prices.count
+      data[:complete_set_completed_listing_new_avg_price] = new_set_sold_prices.mean.round(2)
+      data[:complete_set_completed_listing_new_median_price] = new_set_sold_prices.median.round(2)
+      data[:complete_set_completed_listing_new_high_price] = new_set_sold_prices.max
+      data[:complete_set_completed_listing_new_low_price] = new_set_sold_prices.min
+    end
+    data
+  end
+
+  def self.get_complete_set_last_six_months_sales_used_values set_listings
+    data = {}
+    sold_used_listing_tables = set_listings.css("td:nth-child(2) table")
+    used_set_sold_prices = []
+
+    sold_used_listing_tables.each do |table|
+      first_cell = table.css("td:first-child").first
+      next if (!first_cell.attribute("colspan").nil? && first_cell.attribute("colspan").value.to_i == 3) || table.attribute("cellpadding").value.to_i == 0
+      sold_listing_rows = table.css("table tr")
+      sold_listing_rows.drop(1).each do |r|
+        first_column = r.css("td:first-child")
+        break if !first_column.attribute("colspan").nil? && first_column.attribute("colspan").value.to_i == 3        
+        qty = r.css("td:nth-child(2)").text.to_i
+        value = BricklinkService.c_to_f(r.css("td:last-child").text)
+        qty.times do 
+          used_set_sold_prices << value
+        end
+      end
+    end
+
+    data[:complete_set_completed_listing_used_listings_count] = 0
+    unless used_set_sold_prices.empty?
+      data[:complete_set_completed_listing_used_listings_count] = used_set_sold_prices.count
+      data[:complete_set_completed_listing_used_avg_price] = used_set_sold_prices.mean.round(2)
+      data[:complete_set_completed_listing_used_median_price] = used_set_sold_prices.median.round(2)
+      data[:complete_set_completed_listing_used_high_price] = used_set_sold_prices.max
+      data[:complete_set_completed_listing_used_low_price] = used_set_sold_prices.min
+    end
+    data
+  end
+
+  def self.get_complete_set_used_values set_listings
+    data = {}
+    current_new_listing_rows = set_listings.css("td:nth-child(4) table:nth-child(3) table tr")
+    used_set_prices = []
+    current_new_listing_rows.drop(1).each do |r|
+      first_column = r.css("td:first-child")
+      break if !first_column.attribute("colspan").nil? && first_column.attribute("colspan").value.to_i == 3        
+
+      qty = r.css("td:nth-child(2)").text.to_i
+      value = BricklinkService.c_to_f(r.css("td:last-child").text)
+      qty.times do 
+        used_set_prices << value
+      end
+    end
+    # used_set_prices
+    data[:complete_set_used_listings_count] = 0
+    unless used_set_prices.empty?
+      data[:complete_set_used_listings_count] = used_set_prices.count
+      data[:complete_set_used_avg_price] = used_set_prices.mean.round(2)
+      data[:complete_set_used_median_price] = used_set_prices.median.round(2)
+      data[:complete_set_used_high_price] = used_set_prices.max
+      data[:complete_set_used_low_price] = used_set_prices.min
+    end
+    data
   end
 end
