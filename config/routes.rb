ab9dd9783d44817718694292c693361e68bf98c8
Kevin Powell <kevinmpowell@gmail.com>
2018-01-31 12:07:46 -0600

    Lock down sidekiq route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,15 @@ Rails.application.routes.draw do
   post 'auth/signin', to: 'authentication#authenticate'
   get 'auth/validate-token', to: 'authentication#validate_token'
 
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    # Protect against timing attacks:
+    # - See https://codahale.com/a-lesson-in-timing-attacks/
+    # - See https://thisdata.com/blog/timing-attacks-against-string-comparison/
+    # - Use & (do not use &&) so that it doesn't short circuit.
+    # - Use digests to stop length information leaking (see also ActiveSupport::SecurityUtils.variable_size_secure_compare)
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_USERNAME"])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV["SIDEKIQ_PASSWORD"]))
+  end
   mount Sidekiq::Web => '/sidekiq'
 
   resources :lego_sets, only: [:index, :show] do
