325d0b3a07c561a16e5d5e373a2b904e3326d7af
Kevin Powell <kevinmpowell@gmail.com>
2018-02-01 00:02:44 -0600

    Add base64 encoding of set data endpoint

--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -1,3 +1,5 @@
+require "base64"
+
 class LegoSetsController < ApplicationController
   skip_before_action :authorize_request, only: [:index, :show]
   before_action :set_lego_set, only: [:show]
@@ -6,7 +8,8 @@ class LegoSetsController < ApplicationController
   def index
     bypass_cache = index_params[:bypass_cache].nil? ? false : index_params[:bypass_cache]
     lego_sets_object = LegoSet.all_sets_as_object(bypass_cache)
-    json_response(lego_sets_object)
+    # json_response(lego_sets_object)
+    render plain: Base64.encode64(lego_sets_object.to_json), status: :ok
   end
 
   # GET /lego_sets/:id
