b85cd0328c966e3157007871e575cc142cc4b56d
Kevin Powell <kevinmpowell@gmail.com>
2018-02-02 12:37:29 -0600

    Lock down all routes to only be accessible via xhr requests

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # TODO: Lock down all these requests behind xhr restrictions
   # TODO: Set CORS Headers at the environment level, so localhost:3000 can't make XHR requests to prod
   post 'subscribe', to: 'subscribers#create', constraints: lambda { |request| request.xhr? }
-  post 'signup', to: 'users#create'
-  post 'auth/signin', to: 'authentication#authenticate'
-  get 'auth/validate-token', to: 'authentication#validate_token'
+  post 'signup', to: 'users#create', constraints: lambda { |request| request.xhr? }
+  post 'auth/signin', to: 'authentication#authenticate', constraints: lambda { |request| request.xhr? }
+  get 'auth/validate-token', to: 'authentication#validate_token', constraints: lambda { |request| request.xhr? }
 
   Sidekiq::Web.use Rack::Auth::Basic do |username, password|
     # Protect against timing attacks:
@@ -20,8 +20,8 @@ Rails.application.routes.draw do
   end
   mount Sidekiq::Web => '/sidekiq'
 
-  resources :lego_sets, only: [:index, :show] do
-  # resources :lego_sets, only: [:index, :show], constraints: lambda { |request| request.xhr? } do # Locks down requests to only work via AJAX
+  # resources :lego_sets, only: [:index, :show] do
+  resources :lego_sets, only: [:index, :show], constraints: lambda { |request| request.xhr? } do # Locks down requests to only work via AJAX
     resources :ebay_sales, only: [:index]
   end
 end
