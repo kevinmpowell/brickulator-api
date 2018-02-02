955941e9dedf6d3c7c0671909f986e7dfd390e30
Kevin Powell <kevinmpowell@gmail.com>
2018-02-02 12:34:39 -0600

    rot encode user prefs

--- app/controllers/lego_sets_controller.rb
+++ app/controllers/lego_sets_controller.rb
@@ -4,16 +4,11 @@ class LegoSetsController < ApplicationController
   skip_before_action :authorize_request, only: [:index, :show]
   before_action :set_lego_set, only: [:show]
 
-  def rot13(s)
-    s.tr('A-Za-z', 'N-ZA-Mn-za-m').tr('1-9', '4-91-3')
-  end
-
   # GET /lego_sets
   def index
     bypass_cache = index_params[:bypass_cache].nil? ? false : index_params[:bypass_cache]
     lego_sets_object = LegoSet.all_sets_as_object(bypass_cache)
-    # json_response(lego_sets_object)
-    render plain: rot13(lego_sets_object.to_json), status: :ok
+    rot13_json_response(lego_sets_object)
   end
 
   # GET /lego_sets/:id
