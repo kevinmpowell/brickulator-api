e71f4ef0ea4e9a50f01505906ee77feb359cdeb6
Kevin Powell <kevinmpowell@gmail.com>
2018-03-09 23:11:57 -0600

    Add brickulator.com to CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:3000', 'https://kevinmpowell.github.io'
+    origins 'https://localhost:3000', 'https://kevinmpowell.github.io', 'http://brickulator.com', 'https://brickulator.com'
 
     resource '*',
       headers: :any,
